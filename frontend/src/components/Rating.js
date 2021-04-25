7c9c860cc1cebaa49c9b2ab6211e2ee7f3a3e7e4
K Vijith <vijith.messi@gmail.com>
2021-04-25 12:17:10 +0530

    Cart Functionalities Added

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+
 const Rating = ({ value, text, color }) => {
   return (
     <div className='rating'>
@@ -8,72 +8,68 @@ const Rating = ({ value, text, color }) => {
           style={{ color }}
           className={
             value >= 1
-              ? 'fa fa-star'
+              ? 'fas fa-star'
               : value >= 0.5
-              ? 'fa fa-star-half-alt'
+              ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
-        />
+        ></i>
       </span>
       <span>
         <i
           style={{ color }}
           className={
             value >= 2
-              ? 'fa fa-star'
+              ? 'fas fa-star'
               : value >= 1.5
-              ? 'fa fa-star-half-alt'
+              ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
-        />
+        ></i>
       </span>
       <span>
         <i
           style={{ color }}
           className={
             value >= 3
-              ? 'fa fa-star'
+              ? 'fas fa-star'
               : value >= 2.5
-              ? 'fa fa-star-half-alt'
+              ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
-        />
+        ></i>
       </span>
       <span>
         <i
           style={{ color }}
           className={
             value >= 4
-              ? 'fa fa-star'
+              ? 'fas fa-star'
               : value >= 3.5
-              ? 'fa fa-star-half-alt'
+              ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
-        />
+        ></i>
       </span>
       <span>
         <i
           style={{ color }}
           className={
             value >= 5
-              ? 'fa fa-star'
+              ? 'fas fa-star'
               : value >= 4.5
-              ? 'fa fa-star-half-alt'
+              ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
-        />
+        ></i>
       </span>
       <span>{text && text}</span>
     </div>
   )
 }
+
 Rating.defaultProps = {
-  color: '#f8e825'
-}
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string
+  color: '#f8e825',
 }
 
 export default Rating
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,32 @@
-import {createStore,combineReducers,applyMiddleware} from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import {productListReducer,productDetailsReducer} from './reducers/productReducers'
-const reducer =combineReducers({
-    productList:productListReducer,
-    productDetails:productDetailsReducer
+import { composeWithDevTools } from 'redux-devtools-extension'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
 
-const initialState={}
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
 
-const middleware=[thunk] 
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+}
 
-const store=createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)))
+const middleware = [thunk]
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
 export default store
