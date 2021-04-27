885bc97106bc1fbedb135271c7d60f87569d6b5f
K Vijith <vijith.messi@gmail.com>
2021-04-27 18:13:22 +0530

    added login and register screens in the front end 😎

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+
 
 const App = () => {
   return (
@@ -16,6 +19,8 @@ const App = () => {
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
         </Container>
       </main>
       <Footer />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,20 +5,28 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers'
+import {userLoginReducer,userRegisterReducer} from './reducers/userReducers'
 import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin:userLoginReducer,
+  userRegister: userRegisterReducer,  
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: { cartItems: cartItemsFromStorage},
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
